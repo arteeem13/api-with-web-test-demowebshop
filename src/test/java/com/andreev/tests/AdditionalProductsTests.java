334edc1f02cd07c97cb626f43d30a921eb665798
qa-artem <artem1305@yandex.ru>
2023-02-01 11:56:57 +0300

    optimized imports

--- src/test/java/com/andreev/tests/AdditionalProductsTests.java
+++ src/test/java/com/andreev/tests/AdditionalProductsTests.java
@@ -9,9 +9,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static io.qameta.allure.Allure.attachment;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
